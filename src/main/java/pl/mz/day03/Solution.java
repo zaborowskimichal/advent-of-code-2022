90a90ae1b2611c98f5b38190c9dc6240b6892839
Michał Zaborowski <zaborowskimchl@gmail.com>
2022-12-03 15:11:02 +0100

    day 3 completed

--- src/main/java/pl/mz/day03/Solution.java
+++ src/main/java/pl/mz/day03/Solution.java
@@ -9,13 +9,31 @@ public class Solution {
 
     public static void main(String[] args) {
         LinkedList<String> fileList = FileProcessor.readFile("src/main/java/pl/mz/day03/input.txt");
-        ArrayList<String[]> backpacks =  convertFileList(fileList);
-        List<Character> letters = compareBakcpacks(backpacks);
-        System.out.println("Part 1: " + letters.stream().mapToInt(Solution::calculateCharValue).sum());
+        for (int i = 1; i < 3; i++) {
+            ArrayList<String[]> backpacks = convertFileList(fileList, i);
+            List<Character> letters = compareBakcpacks(backpacks, i);
+            System.out.println("Part " + i + ": " + letters.stream().mapToInt(Solution::calculateCharValue).sum());
+        }
     }
 
-    public static  ArrayList<String[]> convertFileList(LinkedList<String> fileList) {
-        return (ArrayList<String[]>) fileList.stream().map(Solution::splitBackpack).collect(Collectors.toList());
+    public static ArrayList<String[]> convertFileList(LinkedList<String> fileList, int part) {
+        if (part == 1)
+            return (ArrayList<String[]>) fileList.stream().map(Solution::splitBackpack).collect(Collectors.toList());
+        int counter = 0;
+        ArrayList<String[]> result = new ArrayList<>();
+        String[] temp = new String[3];
+        for (String s : fileList) {
+            temp[counter] = s;
+            if (counter < 2) {
+                counter++;
+            } else {
+                result.add(temp);
+                temp = new String[3];
+                counter = 0;
+            }
+        }
+        if (counter != 0) result.add(temp);
+        return result;
     }
 
 
@@ -23,22 +41,36 @@ public class Solution {
         return new String[]{text.substring(0, text.length() / 2), text.substring(text.length() / 2)};
     }
 
-    public static HashSet<Character> findCommonCharacters(String first, String second){
+    public static HashSet<Character> findCommonCharacters(String first, String second) {
         HashSet<Character> result = new HashSet<>();
         HashSet<Character> charSet = new HashSet<>();
-        for(char c : first.toCharArray()) charSet.add(c);
-        for(char c : second.toCharArray()) if(charSet.contains(c)) result.add(c);
+        for (char c : first.toCharArray()) charSet.add(c);
+        for (char c : second.toCharArray()) if (charSet.contains(c)) result.add(c);
 
         return result;
     }
 
-    public static List<Character> compareBakcpacks(List<String[]> backpacks){
-        return backpacks.stream().map(e -> findCommonCharacters(e[0],e[1]))
+    public static HashSet<Character> findCommonCharacters(String first, String second, String third) {
+        HashSet<Character> commonPart = new HashSet<>();
+        HashSet<Character> charSet = new HashSet<>();
+        HashSet<Character> result = new HashSet<>();
+        for (char c : first.toCharArray()) charSet.add(c);
+        for (char c : second.toCharArray()) if (charSet.contains(c)) commonPart.add(c);
+        for (char c : third.toCharArray()) if (commonPart.contains(c)) result.add(c);
+
+        return result;
+    }
+
+    public static List<Character> compareBakcpacks(List<String[]> backpacks, int part) {
+        if (part == 1) return backpacks.stream().map(e -> findCommonCharacters(e[0], e[1]))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        else return backpacks.stream().map(e -> findCommonCharacters(e[0], e[1], e[2]))
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
     }
 
-    public static int calculateCharValue(Character c){
+    public static int calculateCharValue(Character c) {
         return (int) c - 'a' < 0 ? (int) c - 'A' + 27 : (int) c - 'a' + 1;
     }
 }
